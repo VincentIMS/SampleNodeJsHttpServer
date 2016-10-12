1690595a8733615487757731c681bde0fe205b7b
lsteffens <luc.steffens@persgroep.be>
2016-10-12 18:29:36 +0200

    Hup klaar!

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var busteVerwerker = require('body-parser');
 
+app.use(busteVerwerker.json());
 
-app.get("/book", function(request, response){
-  response.send({name:"Beginning Node JS", rating:4});
+var books = [];
+books.push({id:1, name:"Beginning Node JS", rating:4});
+books.push({id:2, name:"Knoeien met data-distributie", rating:3});
+
+app.get("/books", function(request, response){
+  response.send(books);
+});
+
+app.post("/books", function(request, response){
+  var book= request.body;
+  book.id= books.length+1;
+  books.push(book);
+  response.status(204).send();
 });
 
 app.listen(4567);
