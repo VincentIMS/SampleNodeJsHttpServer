73f344697eb877df89c634619eba5f11ea205d49
lsteffens <luc.steffens@persgroep.be>
2016-10-12 17:34:03 +0200

    We hebben een GET!!

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var app = express();
 
-console.log("woow, dees werkt!");
+
+app.get("/book", function(request, response){
+  response.send({name:"Beginning Node JS", rating:4});
+});
+
+app.listen(4567);
+console.log("woow, dees werkt! Maar het doet echt niks..");
